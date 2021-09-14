c106ad9b7245cd569ab6cf5793db3af41a753ee6
Minjun Kang <minjun@kaaang.dev>
2021-09-14 16:11:15 +0900

    feat: 404 페이지 대신 index.html를 렌더하라

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 const domain = process.env.DOMAIN || 'localhost';
 
@@ -9,7 +10,15 @@ const vhost = require('vhost');
 app.use(vhost(`*.${domain}`, (req, res) => {
   const subdomain = req.vhost[0].replace(/[^\dA-Za-z]/g, '');
 
-  res.sendFile(path.normalize(req.originalUrl), { root: path.join('commits', subdomain) });
+  try {
+    if (fs.existsSync(path.join('commits', subdomain, req.originalUrl))) {
+      res.sendFile(path.normalize(req.originalUrl), { root: path.join('commits', subdomain) });
+    } else {
+      res.sendFile('index.html', { root: path.join('commits', subdomain) });
+    }
+  } catch (err) {
+    console.error(err);
+  }
 }));
 
 app.listen(process.env.PORT || 3000, () => {
